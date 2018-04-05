3cb011f2cc93a13f37a8e147fb14522a34fde3e4
davidrud135 <david.rud135@gmail.com>
2018-04-05 21:55:02 +0300

    Added Header auto padding JS file

---
+++ js/Header-AutoPadding-Animation.js
@@ -0,0 +1,12 @@
+$(document).ready(() => {
+  setInterval(() => {
+    const windowHeight = $(window).height();
+    const containerHeight = $('.header .container:last').height();
+    const paddingTop = windowHeight - containerHeight;
+
+    $('.header .container:last').css({
+      'padding-top': `${Math.round(paddingTop / 2)}px`,
+      'padding-bottom': `${Math.round(paddingTop / 2)}px`,
+    });
+  }, 10);
+});
