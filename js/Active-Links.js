ab6d517165e5c5ec122cf29233251df00e05a538
davidrud135 <david.rud135@gmail.com>
2018-04-10 14:59:02 +0300

    Refactored Active-Links file

--- js/Active-Links.js
+++ js/Active-Links.js
@@ -1,26 +1,35 @@
-/* eslint-disable func-names */
-$(document).ready(() => {
-  $('.navbar-nav li a').click(function () {
-    $('.navbar-nav li a').parent().removeClass('active');
-    $(this).parent().addClass('active');
-  });
+(function () {
+  const $navbarLinksSelector = $('.navbar-nav li a'),
+    $window = $(window);
 
-  $(window).scroll(() => {
-    $('section').each(function () {
-      const currSectionId = $(this).attr('id');
-      const currSectionHeight = $(this).outerHeight();
-      const currSectionPosition = $(this).offset().top - 70;
+  function onLinkClick() {
+    const $this = $(this);
+    $navbarLinksSelector.parent().removeClass('active');
+    $this.parent().addClass('active');
+  };
 
-      if (
-        $(window).scrollTop() > currSectionPosition
-        && $(window).scrollTop() < currSectionPosition + currSectionHeight
-      ) {
-        $(`.navbar-nav li a[href="#${currSectionId}"]`).parent()
-          .addClass('active');
-      } else {
-        $(`.navbar-nav li a[href="#${currSectionId}"]`).parent()
-          .removeClass('active');
-      }
+  function highlightCurrLink() {
+    const $this = $(this),
+      $currSectionId = $this.attr('id'),
+      $currSectionHeight = $this.outerHeight(),
+      $currSectionPositionTop = $this.offset().top - 70,
+      $currSectionLinkSelector =
+        $(`.navbar-nav li a[href="#${$currSectionId}"]`);
+
+    if(
+      $window.scrollTop() > $currSectionPositionTop
+      && $window.scrollTop() < $currSectionPositionTop + $currSectionHeight
+    ) {
+      $currSectionLinkSelector.parent().addClass('active');
+    } else {
+      $currSectionLinkSelector.parent().removeClass('active');
+    }
+  };
+
+  $(document).ready(() => {
+    $navbarLinksSelector.on('click', onLinkClick);
+    $window.on('scroll', () => {
+      $('section').each(highlightCurrLink);
     });
   });
-});
+}(jQuery));
