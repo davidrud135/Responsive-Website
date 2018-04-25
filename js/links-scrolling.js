7ab1d23294484a1917fdbb2b341b292a00c0b409
davidrud135 <david.rud135@gmail.com>
2018-04-25 18:55:14 +0300

    Refactoring JS file

--- js/links-scrolling.js
+++ js/links-scrolling.js
@@ -1,31 +1,10 @@
 (function () {
-  function scroll(event) {
-    if (
-      window.location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
-      window.location.hostname === this.hostname
-    ) {
-      let target = $(this.hash);
-      target = target.length ? target : $(`[name=${this.hash.slice(1)}]`);
-      if (target.length) {
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top,
-        }, 1000, () => {
-          const $target = $(target);
-          $target.focus();
-          if ($target.is(':focus')) {
-            return false;
-          }
-          $target.attr('tabindex', '-1');
-          $target.focus();
-          return 0;
-        });
-      }
-    }
+  const $navbarLinks = $('nav li a');
+  function scrollToNeededSection() {
+    let targetSection = $(this.hash);
+    $('html, body').animate({
+      scrollTop: targetSection.offset().top
+    }, 1000);
   }
-
-  $(document).ready(() => {
-    const $allLinksSelector = $('a[href*="#"]');
-    $allLinksSelector.click(scroll);
-  });
+  $navbarLinks.click(scrollToNeededSection);
 }(jQuery));
