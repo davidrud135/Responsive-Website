cabffd327f31568236b74517330451b268df2f9e
davidrud135 <david.rud135@gmail.com>
2018-04-27 00:05:51 +0300

    Inline CSS styles was separated to another file

--- js/navbar-animation.js
+++ js/navbar-animation.js
@@ -1,30 +1,14 @@
 (function () {
   const $window = $(window);
+  const $navbar = $('.navbar');
+
   function hideNavbar() {
-    $('.navbar').css({
-      'margin-top': '-100px',
-      opacity: '0',
-    });
-    $('.navbar-default').css({
-      'background-color': 'rgba(59, 59, 59, 0)',
-    });
+    $navbar.removeClass('show');
+    $navbar.addClass('hide');
   }
   function showNavbar() {
-    $('.navbar').css({
-      'margin-top': '0px',
-      opacity: '1',
-    });
-    $('.navbar-default').css({
-      'background-color': 'rgba(59, 59, 59, 0.7)',
-      'border-color': '#444',
-    });
-    $('.navbar-brand img').css({
-      height: '35px',
-      'padding-top': '0px',
-    });
-    $('.navbar-nav > li > a').css({
-      'padding-top': '15px',
-    });
+    $navbar.removeClass('hide');
+    $navbar.addClass('show');
   }
 
   $(document).ready(() => {
