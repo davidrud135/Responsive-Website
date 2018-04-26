60acc8d154d90d99f7039acbc61aee74520d2a30
davidrud135 <david.rud135@gmail.com>
2018-04-26 16:02:18 +0300

    Refactored JS file

--- js/navbar-animation.js
+++ js/navbar-animation.js
@@ -1,32 +1,38 @@
 (function () {
+  const $window = $(window);
+  function hideNavbar() {
+    $('.navbar').css({
+      'margin-top': '-100px',
+      opacity: '0',
+    });
+    $('.navbar-default').css({
+      'background-color': 'rgba(59, 59, 59, 0)',
+    });
+  }
+  function showNavbar() {
+    $('.navbar').css({
+      'margin-top': '0px',
+      opacity: '1',
+    });
+    $('.navbar-default').css({
+      'background-color': 'rgba(59, 59, 59, 0.7)',
+      'border-color': '#444',
+    });
+    $('.navbar-brand img').css({
+      height: '35px',
+      'padding-top': '0px',
+    });
+    $('.navbar-nav > li > a').css({
+      'padding-top': '15px',
+    });
+  }
+
   $(document).ready(() => {
-    const $window = $(window);
     $window.scroll(() => {
-      if ($window.scrollTop() < 80) {
-        $('.navbar').css({
-          'margin-top': '-100px',
-          opacity: '0',
-        });
-        $('.navbar-default').css({
-          'background-color': 'rgba(59, 59, 59, 0)',
-        });
-      } else {
-        $('.navbar').css({
-          'margin-top': '0px',
-          opacity: '1',
-        });
-        $('.navbar-default').css({
-          'background-color': 'rgba(59, 59, 59, 0.7)',
-          'border-color': '#444',
-        });
-        $('.navbar-brand img').css({
-          height: '35px',
-          'padding-top': '0px',
-        });
-        $('.navbar-nav > li > a').css({
-          'padding-top': '15px',
-        });
-      }
+      if ($window.scrollTop() < 80)
+        hideNavbar();
+      else
+        showNavbar();
     });
   });
 }(jQuery));
